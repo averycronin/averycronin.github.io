e89bb0b3f4d2c38a6c2075d2273753a16ff71f4a
Avery Cronin <94068561+averycronin@users.noreply.github.com>
2023-12-06 17:21:29 -0500

    fixed for final exam 587

--- files/red.py
+++ files/red.py
@@ -9,6 +9,9 @@ for line in sys.stdin:
    key, value = line.split("\t")
 
    value = float(value)
+   
+   if key == "goo":
+       break
 
     # value : count : min : max
    # if this is the first iteration
@@ -17,15 +20,19 @@ for line in sys.stdin:
 
    else: 
         temp = mydict[key].split(":")
-        if value < temp[2]:
-            mydict[key] = str(float(temp[0]) + value) + ":" + str(float(temp[1]) + 1 + ":" + str(value) + ":" + temp[3])
-        elif value > temp[3]:
-            mydict[key] = str(float(temp[0]) + value) + ":" + str(float(temp[1]) + 1 + ":" + temp[2] + ":" + str(value))
-        else:
-            mydict[key] = str(float(temp[0]) + value) + ":" + str(float(temp[1]) + 1 + ":" + temp[2] + ":" + temp[3])
+        
+        temp = mydict[key].split(":")
+        min = str(value) if value < float(temp[2]) else temp[2]
+        max = str(value) if value > float(temp[3]) else temp[3]
+        mydict[key] = str(float(temp[0]) + value) + ":" + str(float(temp[1]) + 1) + ":" + min + ":" + max
 
 # Airline-Origin    Min     Max     Average
 for key in mydict:
     ave = mydict[key].split(":")
     aver = float(ave[0]) / float(ave[1])
     print(key + "\t" + ave[2] + "\t" + ave[3] + "\t" + str(aver))
+    
+#temp = mydict[key].split(":")
+ #       min = str(value) if value < float(temp[2]) else temp[2]
+  #      max = str(value) if value > float(temp[3]) else temp[3]
+   #     mydict[key] = str(float(temp[0]) + value) + ":" + str(float(temp[1]) + 1 + ":" + min + ":" + max)
