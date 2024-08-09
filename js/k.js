3b5155838e804ba2986665b2458696c69c2ec8d6
averycronin <averywcronin@gmail.com>
2024-08-09 10:05:47 -0400

    accessibility + bug fix

--- js/k.js
+++ js/k.js
@@ -1,32 +1,31 @@
 'use strict';
 
 let input = [];
-let keys;
 
 document.addEventListener('keydown', event => {
 	const charList = 'ab';
 	const key = event.key.toLowerCase();
 	if (!(charList.includes(key)) && (key !== 'arrowup') && (key !== 'arrowdown') && (key !== 'arrowright') && (key !== 'arrowleft')) { return; }
 
-    keys = input.length;
+    let keys = input.length;
 
     switch(keys) {
         case 0:
-        case 1: key === 'arrowup' ? keys.push(key) : keys = []
+        case 1: key === 'arrowup' ? input.push(key) : input = []
         break;
         case 2: 
-        case 3: key === 'arrowdown' ? keys.push(key) : keys = []
+        case 3: key === 'arrowdown' ? input.push(key) : input = []
         break;
         case 4: 
-        case 6: key === 'arrowleft' ? keys.push(key) : keys = []
+        case 6: key === 'arrowleft' ? input.push(key) : input = []
         break;
         case 5: 
-        case 7: key === 'arrowright' ? keys.push(key) : keys = []
+        case 7: key === 'arrowright' ? input.push(key) : input = []
         break;
-        case 8: key === 'b' ? keys.push(key) : keys = []
+        case 8: key === 'b' ? input.push(key) : input = []
         break;
-        case 9: key === 'a' ? window.location.href = "https://averycronin.com/game" : keys = []
+        case 9: key === 'a' ? window.location.href = "https://averycronin.com/game" : input = []
         break;
-        default: keys = []
+        default: input = []
     }
 });
