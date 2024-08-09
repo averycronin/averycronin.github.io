88d47ceec000673eb0eea8f00cbea186d38a31f8
averycronin <averywcronin@gmail.com>
2024-08-09 09:36:30 -0400

    Easter Egg

---
+++ js/k.js
@@ -0,0 +1,31 @@
+'use strict';
+
+input = [];
+
+document.addEventListener('keydown', event => {
+	const charList = 'ab';
+	const key = event.key.toLowerCase();
+	if (!(charList.includes(key)) && (key !== 'arrowup') && (key !== 'arrowdown') && (key !== 'arrowright') && (key !== 'arrowleft')) { return; }
+
+    keys = input.length;
+
+    switch(keys) {
+        case 0:
+        case 1: key === 'arrowup' ? keys.push(key) : keys = []
+        break;
+        case 2: 
+        case 3: key === 'arrowdown' ? keys.push(key) : keys = []
+        break;
+        case 4: 
+        case 6: key === 'arrowleft' ? keys.push(key) : keys = []
+        break;
+        case 5: 
+        case 7: key === 'arrowright' ? keys.push(key) : keys = []
+        break;
+        case 8: key === 'b' ? keys.push(key) : keys = []
+        break;
+        case 9: key === 'a' ? window.location.href = "https://averycronin.com/game" : keys = []
+        break;
+        default: keys = []
+    }
+});
